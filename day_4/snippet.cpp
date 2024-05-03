e74d2f28391ff836e0ac2747b2633bdbb91f93eb
SAMIUL2022 <sami35bd@gmail.com>
2024-05-03 22:25:39 +0600

    .

---
+++ day_4/snippet.cpp
@@ -0,0 +1,38 @@
+// B M SAMIUL
+#include <bits/stdc++.h>
+using namespace std;
+#define op()                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl cout << '\n';
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define PI 2.0 * acos(0.0)
+#define Dpos(n) fixed << setprecision(n)
+#define lpi(j) for(int i=0; i<n; i++) cin>>j[i]
+#define lpo(j) for(int i=0; i<n; i++) cout<<j[i]<<" ";
+#define sort(j) for(j.begin(),j.end()) ;
+#define sortg(j) for(j.begin(),j.end(),greater<int>()) ;
+ 
+typedef long long ll;
+typedef vector<ll> vl;
+const ll N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+ 
+void sami()
+{
+
+}
+int main()
+{
+    op() int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+       
+        sami();
+    }
+}
